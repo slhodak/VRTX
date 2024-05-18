0246d55f1f0a2ad76482e26f7ea523efcdcde6dc
Sam Hodak <slhodak@gmail.com>
2024-05-18 15:21:17 -0500

    use simd_float4x4(1) instead of matrix identity

--- VRTX/Renderer.swift
+++ VRTX/Renderer.swift
@@ -139,7 +139,7 @@ class Renderer: NSObject, MTKViewDelegate {
             
             logger.debug("Projection matrix set with fov: \(self.perspectiveFOVRadians()), aspect: \(self.projectionPerspectiveAspect), near: \(self.projectionNear), far: \(self.projectionFar)")
         } else {
-            projectionMatrix = matrix_identity_float4x4
+            projectionMatrix = simd_float4x4(1)
         }
         
         projectionMatrixBuffer = device.makeBuffer(bytes: &projectionMatrix,
