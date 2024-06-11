7e30e6c9c5a8ca44298fcad01b262a68e73a0979
Sam Hodak <slhodak@gmail.com>
2024-06-11 09:19:30 -0700

    use sliders for light color

--- VRTX/Material/MaterialUIView.swift
+++ VRTX/Material/MaterialUIView.swift
@@ -4,15 +4,18 @@ struct MaterialUIView: View {
     @State var material: Material
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Materials").bold()
-            VectorView(name: "Specular Color",
-                       vector: $material.specularColor,
-                       labels: ["r", "g", "b"])
+            Text("Specular Color")
+            LabeledSlider(name: "R", value: $material.specularColor.x, min: 0, max: 1)
+            LabeledSlider(name: "G", value: $material.specularColor.y, min: 0, max: 1)
+            LabeledSlider(name: "B", value: $material.specularColor.z, min: 0, max: 1)
             LabeledSlider(name: "Specular Power",
                           value: $material.specularPower,
-                          min: 0,
+                          min: 0.01,
                           max: 300)
+            Spacer()
         }
+        .padding()
     }
 }
