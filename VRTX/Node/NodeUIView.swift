488c405ac27572879d8164ec4a3f7fa86b1dbb50
Sam Hodak <slhodak@gmail.com>
2024-06-12 07:31:21 -0700

    Make node views collapsible

--- VRTX/Node/NodeUIView.swift
+++ VRTX/Node/NodeUIView.swift
@@ -4,21 +4,38 @@ import os
 
 struct NodeUIView: View {
     @State var node: Node
+    @State var isExpanded = true
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(node.name)
-            LabeledSlider(name: "ScaleX", value: $node.scale.x, min: 0, max: 10)
-            LabeledSlider(name: "ScaleY", value: $node.scale.y, min: 0, max: 10)
-            LabeledSlider(name: "ScaleZ", value: $node.scale.z, min: 0, max: 10)
-            LabeledSlider(name: "TranslateX", value: $node.translation.x, min: -10, max: 10)
-            LabeledSlider(name: "TranslateY", value: $node.translation.y, min: -10, max: 10)
-            LabeledSlider(name: "TranslateZ", value: $node.translation.z, min: -10, max: 10)
-            VectorView(name: "Rotation Axis", vector: $node.rotationAxis)
-            LabeledSlider(name: "RotationAngle", value: $node.rotationAngle, min: 0, max: 2)
+            HStack {
+                Text(node.name)
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .frame(width: 10, height: 10)
+                    .rotationEffect(.degrees(isExpanded ? 0 : 90))
+                    .onTapGesture {
+                        isExpanded.toggle()
+                    }
+            }
             
-            Text("Transformed Model Matrix")
-            Matrix4x4View(mat: $node.localTransform)
+            if isExpanded {
+                LabeledSlider(name: "ScaleX", value: $node.scale.x, min: 0, max: 10)
+                LabeledSlider(name: "ScaleY", value: $node.scale.y, min: 0, max: 10)
+                LabeledSlider(name: "ScaleZ", value: $node.scale.z, min: 0, max: 10)
+                LabeledSlider(name: "TranslateX", value: $node.translation.x, min: -10, max: 10)
+                LabeledSlider(name: "TranslateY", value: $node.translation.y, min: -10, max: 10)
+                LabeledSlider(name: "TranslateZ", value: $node.translation.z, min: -10, max: 10)
+                VectorView(name: "Rotation Axis", vector: $node.rotationAxis)
+                LabeledSlider(name: "RotationAngle", value: $node.rotationAngle, min: 0, max: 2)
+                
+                Text("Transformed Model Matrix")
+                Matrix4x4View(mat: $node.localTransform)
+                
+                if let node = node as? CustomNode {
+                    CustomNodeUIView(geometry: node.geometry)
+                }
+            }
         }
     }
 }
@@ -46,9 +63,6 @@ struct NodesView: View {
             Text("Nodes").bold()
             ForEach(renderer.rootNode.children) { node in
                 NodeUIView(node: node)
-                if let node = node as? CustomNode {
-                    CustomNodeUIView(geometry: node.geometry)
-                }
                 Divider()
             }
         }.padding()
